2247173f99a0d28d88152db0123d881ad9d70a39
Jimut Bahan Pal <jimutbahanpal@yahoo.com>
2021-03-10 11:58:45 +0530

    blob detector from grayscale doesn't work as expected

--- Detector/MSBlobDetector/Blob_Detector_Grayscale.py
+++ Detector/MSBlobDetector/Blob_Detector_Grayscale.py
@@ -21,44 +21,12 @@ all_imgs = glob.glob('../samples/*')
 
 for im_name in all_imgs:
     # im = cv2.imread(all_imgs[3], cv2.IMREAD_UNCHANGED)
-    im = cv2.imread(im_name, cv2.IMREAD_UNCHANGED)
-    im = cv2.resize(im, (360, 360))
-    src = im
-
-    scale = 1
-    delta = 0
-    fig = plt.figure()
-    counter = 0
-
-    sobel_imgs = []
-    for i_loop in range(10):
-        for j_loop in range(10):
-            if i_loop == j_loop and i_loop > 0:
-                scale = i_loop
-                delta = j_loop
-                ddepth = cv2.CV_16S
-                src = cv2.GaussianBlur(src, (3, 3), 0)
-                gray = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
-                grad_x = cv2.Sobel(gray, ddepth, 1, 0, ksize=3, scale=scale, delta=delta, borderType=cv2.BORDER_DEFAULT)
-                # Gradient-Y
-                # grad_y = cv2.Scharr(gray,ddepth,0,1)
-                grad_y = cv2.Sobel(gray, ddepth, 0, 1, ksize=3, scale=scale, delta=delta, borderType=cv2.BORDER_DEFAULT)
-                abs_grad_x = cv2.convertScaleAbs(grad_x)
-                abs_grad_y = cv2.convertScaleAbs(grad_y)
-                grad = cv2.addWeighted(abs_grad_x, 0.5, abs_grad_y, 0.5, 0)
-                counter += 1
-                ax1 = fig.add_subplot(3,3,counter)
-                sobel_imgs.append(grad)
-                ax1.imshow(grad, cmap="gray")
-    plt.show()
-
-
-
+    img = cv2.imread(im_name, cv2.IMREAD_UNCHANGED)
+    img = cv2.resize(img, (360, 360))
+    im = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) 
     # https://scikit-image.org/docs/dev/auto_examples/features_detection/plot_blob.html
 
-    # for imgs in sobel_imgs:
-    imgs = sobel_imgs[2]
-    imgs = cv2.GaussianBlur(imgs,(5,5),0)
+    imgs = cv2.GaussianBlur(im,(5,5),0)
     
     image = imgs
     image_gray = imgs
@@ -91,7 +59,7 @@ for im_name in all_imgs:
 
     for idx, (blobs, color, title) in enumerate(sequence):
         ax[idx].set_title(title)
-        ax[idx].imshow(image)
+        ax[idx].imshow(image,cmap='gray')
         for blob in blobs:
             y, x, r = blob
             c = plt.Circle((x, y), r, color=color, linewidth=2, fill=False)
