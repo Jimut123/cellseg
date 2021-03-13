a3d0033d425a6e88587b6c7c0cf49bb4129a85d3
tamalmj <tamal.rkm>
2021-03-13 17:18:33 +0530

    v2 segmodel added

--- Detector/Version_2_Detector/train_SegModel_v2.py
+++ Detector/Version_2_Detector/train_SegModel_v2.py
@@ -158,8 +158,8 @@ def get_data_generator(df, indices, for_training, batch_size=16):
 
 from tensorflow.keras.callbacks import ModelCheckpoint
 from tensorflow import keras
-batch_size = 120
-valid_batch_size = 120
+batch_size = 90
+valid_batch_size = 90
 train_gen = get_data_generator(df, train_idx, for_training=True, batch_size=batch_size)
 valid_gen = get_data_generator(df, valid_idx, for_training=True, batch_size=valid_batch_size)
 
@@ -174,23 +174,23 @@ tensorboard_callback = keras.callbacks.TensorBoard(log_dir=logdir)
 
 history = model.fit(train_gen,
                     steps_per_epoch=len(train_idx)//batch_size,
-                    epochs=200,
+                    epochs=100,
                     callbacks=[tensorboard_callback,callbacks],
                     validation_data=valid_gen,
                     validation_steps=len(valid_idx)//valid_batch_size)
 
 import pandas as pd
 hist_df = pd.DataFrame(history.history) 
-hist_json_file = 'history_classification_model_v2_200e.json' 
+hist_json_file = 'history_classification_model_v2_100e.json' 
 with open(hist_json_file, mode='w') as f:
     hist_df.to_json(f)
 
 # download the model in computer for later use
-model.save('classification_model_v2_blood_200epochs.h5')
+model.save('classification_model_v2_blood_100epochs.h5')
 
 
 from tensorflow import keras
-model = keras.models.load_model('classification_model_v2_blood_200epochs.h5')
+model = keras.models.load_model('classification_model_v2_blood_100epochs.h5')
 
 
 test_gen = get_data_generator(df, test_idx, for_training=False, batch_size=128)
