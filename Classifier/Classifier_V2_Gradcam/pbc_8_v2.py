d9043dddd860b23f4d4aac7d4076a4af32b6fbae
Jimut Bahan Pal <jimutbahanpal@yahoo.com>
2021-05-09 19:28:27 +0530

    pbc 8 gitignore

--- Classifier/Classifier_V2_Gradcam/pbc_8_v2.py
+++ Classifier/Classifier_V2_Gradcam/pbc_8_v2.py
@@ -227,8 +227,8 @@ from tensorflow.keras.callbacks import ModelCheckpoint
 from tensorflow import keras
 # batch_size = 100
 # valid_batch_size = 32
-batch_size = 20
-valid_batch_size = 20
+batch_size = 2
+valid_batch_size = 2
 train_gen = get_data_generator(df, train_idx, for_training=True, batch_size=batch_size)
 valid_gen = get_data_generator(df, valid_idx, for_training=True, batch_size=valid_batch_size)
 
