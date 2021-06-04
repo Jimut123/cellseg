26b24d38dfe3941c76d92bb0bf73daf1f117b3c1
Jimut Bahan Pal <jimutbahanpal@yahoo.com>
2021-06-04 23:35:13 +0530

    msexy masks added

--- Aniket_MASK_RCNN/MaskRCNN_3/generate_mask_from_maskrcnn.py
+++ Aniket_MASK_RCNN/MaskRCNN_3/generate_mask_from_maskrcnn.py
@@ -14,7 +14,7 @@ from PIL import Image, ImageDraw
 vibrant_colors = [[0,0,255], [0,255,0], [0,255,255], [255,0,0], [255,0,255], [255,255,0]]
 
 
-with open('data_test_smear/results.json') as json_data:
+with open('data_test_smear_mixed/results.json') as json_data:
     all_json_annotations = json.load(json_data)
     json_data.close()
 
@@ -25,7 +25,8 @@ for data in all_json_annotations['data']:
     file_name = all_json_annotations['data'][data]['filename']
     height = all_json_annotations['data'][data]['height']
     width = all_json_annotations['data'][data]['width']
-    valid_image_name = "data_test_smear/"+file_name
+    valid_image_name = "data_test_smear_mixed/"+file_name
+    
     img = Image.open(valid_image_name).convert("RGBA")
     # print("Max of Image = ",np.array(img).max())
     
@@ -40,7 +41,7 @@ for data in all_json_annotations['data']:
     get_all_masks = []
 
     for item in all_json_annotations['data'][data]['masks']:
-        
+
         # print(item)
         class_name = item['class_name']
         print("Class Name = ", class_name)
@@ -91,30 +92,36 @@ for data in all_json_annotations['data']:
         # plt.show()
 
         get_all_masks.append(green_mask)
-
         get_bbox_coords.append([x1,y1,w,h,label,col])
     
-    print("Image = img",float(1/len(get_all_masks)))
     # plt.imshow(img[:,:,0])
     # plt.show()
     # plt.imshow(img[:,:,1])
     # plt.show()
-    # plt.imshow(img[:,:,2])
+    # plt.imshow(get_image)
     # plt.show()
     print("Shape of Get Image = ",get_image.shape)
-    final_masked_im = np.zeros_like(get_image)
-    final_masked_im = float(1/len(get_all_masks))*get_image
-    plt.imshow(final_masked_im)
-    plt.show()
+    print(float(1/(len(get_all_masks)+1)))
+    print("Max and Min of get_image = ",get_image.max(),get_image.min())
+    final_masked_im = np.zeros(get_image.shape)
+    final_masked_im = float(1/(len(get_all_masks)+1))*get_image
+    #print("giu",np.unique(get_image)[:50])
+    #print("fmiu",np.unique(final_masked_im)[:50])
+    # print("Max and Min of final_masked_im = ",final_masked_im.max(),final_masked_im.min())
+    
+    # plt.imshow(final_masked_im.astype("uint8"))
+    # plt.show()
     print("fff",final_masked_im.max())
 
     print("len get all mask = ",len(get_all_masks))
     for image in get_all_masks:
+        # plt.imshow(image)
+        # plt.show()
         print("max = ",image.max(),"min = ",image.min())
-        final_masked_im = final_masked_im + float(1/len(get_all_masks))*image
+        final_masked_im = final_masked_im + float(1/(len(get_all_masks)+1))*image
         print("Final max = ",final_masked_im.max(),"min = ",final_masked_im.min())
-        plt.imshow(final_masked_im[:,:,::-1])
-        plt.show()
+        # plt.imshow(final_masked_im[:,:,::-1])
+        # plt.show()
     print("fin = ",final_masked_im.max())
 
     for items in get_bbox_coords:
@@ -129,8 +136,10 @@ for data in all_json_annotations['data']:
         cv2.rectangle(final_masked_im, (x,y), (x+w,y+h), col,15)
         # img, text, coord, type of font, size, col, thickness
         cv2.putText(final_masked_im, str(name), (x, y), 0, 3, [0,0,0], 10)
-    plt.imshow(final_masked_im)
-    plt.show()
+    save_im_name = valid_image_name.split('.')[0]+"_mask_rcnn.jpg"
+    cv2.imwrite(save_im_name,final_masked_im[:,:,::-1])
+    # plt.imshow(final_masked_im)
+    # plt.show()
     # mask_gt_save_name = get_im_path.split('.')[0]+"_gt.jpg"
     # cv2.imwrite(mask_gt_save_name,final_masked_im)
 
