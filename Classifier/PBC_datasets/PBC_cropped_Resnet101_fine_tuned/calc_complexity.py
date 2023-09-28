07434f69f5f568a72bd96a1477250112dca219dc
Jimut Bahan Pal <jimutbahanpal@yahoo.com>
2023-09-28 14:50:54 +0530

    added

--- Classifier/PBC_datasets/PBC_cropped_Resnet101_fine_tuned/calc_complexity.py
+++ Classifier/PBC_datasets/PBC_cropped_Resnet101_fine_tuned/calc_complexity.py
@@ -250,6 +250,45 @@ tf.compat.v1.reset_default_graph()
 
 
 
+def get_model_memory_usage(batch_size, model):
+    import numpy as np
+    try:
+        from keras import backend as K
+    except:
+        from tensorflow.keras import backend as K
+
+    shapes_mem_count = 0
+    internal_model_mem_count = 0
+    for l in model.layers:
+        layer_type = l.__class__.__name__
+        if layer_type == 'Model':
+            internal_model_mem_count += get_model_memory_usage(batch_size, l)
+        single_layer_mem = 1
+        out_shape = l.output_shape
+        if type(out_shape) is list:
+            out_shape = out_shape[0]
+        for s in out_shape:
+            if s is None:
+                continue
+            single_layer_mem *= s
+        shapes_mem_count += single_layer_mem
+
+    trainable_count = np.sum([K.count_params(p) for p in model.trainable_weights])
+    non_trainable_count = np.sum([K.count_params(p) for p in model.non_trainable_weights])
+
+    number_size = 4.0
+    if K.floatx() == 'float16':
+        number_size = 2.0
+    if K.floatx() == 'float64':
+        number_size = 8.0
+
+    total_memory = number_size * (batch_size * shapes_mem_count + trainable_count + non_trainable_count)
+    gbytes = np.round(total_memory / (1024.0 ** 3), 3) + internal_model_mem_count
+    return gbytes
+
+get_memory_usage =  get_model_memory_usage(batch_size=16, model)
+
+
 with open("COMPLEXITY_DUMP.txt", 'a') as f:
     f.write("Total Convolutional Layers: "+str(conv_layer_count)+'\n')
     f.write("Total Linear (Dense) Layers: "+str(linear_layer_count)+'\n')
@@ -257,7 +296,8 @@ with open("COMPLEXITY_DUMP.txt", 'a') as f:
     f.write("Trainable params: "+str(trainable_count)+'\n')
     f.write("Non-trainable params: "+str(non_trainable_count)+'\n')
     f.write("FLOPs (total float operations): "+str(flops.total_float_ops)+"\n")
-    f.write("Full flop report \n"+str(flops)+"\n")
+    # f.write("Full flop report \n"+str(flops)+"\n")
+    f.write("Total memory usage: "+str(get_memory_usage)+"\n")
     f.close()
     
 
