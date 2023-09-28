5cd44a7dbd98da5ccadde1d8aa41dd3e00262c41
Jimut Bahan Pal <jimutbahanpal@yahoo.com>
2023-09-28 16:35:31 +0530

    a

--- Classifier/PBC_datasets/RUN_ALL.sh
+++ Classifier/PBC_datasets/RUN_ALL.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-# cd PBC_cropped_Resnet101_fine_tuned
-python3 PBC_cropped_Resnet101_fine_tuned/calc_complexity.py
-# cd ..
+cd PBC_cropped_Resnet101_fine_tuned
+python3 calc_complexity.py
+cd ..
