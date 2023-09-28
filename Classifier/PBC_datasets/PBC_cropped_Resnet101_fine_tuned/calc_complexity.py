3ab70848a96fee91e1e3ec768cf47876bea1afc7
Jimut Bahan Pal <jimutbahanpal@yahoo.com>
2023-09-28 16:40:03 +0530

    a

--- Classifier/PBC_datasets/PBC_cropped_Resnet101_fine_tuned/calc_complexity.py
+++ Classifier/PBC_datasets/PBC_cropped_Resnet101_fine_tuned/calc_complexity.py
@@ -73,7 +73,7 @@ total_img_names = []
 short_labels = []
 for item in dir:
   print(item)
-  img_names = glob.glob("../{}/*".format(item))[:5]
+  img_names = glob.glob("{}/*".format(item))[:5]
   print("img names = ",img_names[:10])
   short_name = str(img_names[0].split('.')[0]).split('/')[2].split('_')[0]
   short_index[short_name] = img_names[0].split('/')[1]
@@ -467,71 +467,4 @@ for i in tqdm(test_idx):
     y_pred_list.append(int(tf.math.argmax(y_pred, axis=-1)))
     y_test_list.append(index[label])
 
-
-
-from sklearn.metrics import classification_report, confusion_matrix
-matrix = confusion_matrix(y_test_list, y_pred_list)
-report = classification_report(y_test_list, y_pred_list)
-
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-from sklearn.metrics import confusion_matrix
-
-
-
-def cm_analysis(y_true, y_pred, labels, ymap=None, figsize=(10,10)):
-    """
-    Generate matrix plot of confusion matrix with pretty annotations.
-    The plot image is saved to disk.
-    args: 
-      y_true:    true label of the data, with shape (nsamples,)
-      y_pred:    prediction of the data, with shape (nsamples,)
-      filename:  filename of figure file to save
-      labels:    string array, name the order of class labels in the confusion matrix.
-                 use `clf.classes_` if using scikit-learn models.
-                 with shape (nclass,).
-      ymap:      dict: any -> string, length == nclass.
-                 if not None, map the labels & ys to more understandable strings.
-                 Caution: original y_true, y_pred and labels must align.
-      figsize:   the size of the figure plotted.
-    """
-    if ymap is not None:
-        y_pred = [ymap[yi] for yi in y_pred]
-        y_true = [ymap[yi] for yi in y_true]
-        labels = [ymap[yi] for yi in labels]
-    cm = confusion_matrix(y_true, y_pred, labels=labels)
-    cm_sum = np.sum(cm, axis=1, keepdims=True)
-    cm_perc = cm / cm_sum.astype(float) * 100
-    annot = np.empty_like(cm).astype(str)
-    nrows, ncols = cm.shape
-    for i in range(nrows):
-        for j in range(ncols):
-            c = cm[i, j]
-            p = cm_perc[i, j]
-            if i == j:
-                s = cm_sum[i]
-                annot[i, j] = '%.1f%%\n%d/%d' % (p, c, s)
-            elif c == 0:
-                annot[i, j] = ''
-            else:
-                annot[i, j] = '%.1f%%\n%d' % (p, c)
-    cm = pd.DataFrame(cm, index=labels, columns=labels)
-    cm.index.name = 'Actual'
-    cm.columns.name = 'Predicted'
-    fig, ax = plt.subplots(figsize=figsize)
-    sns.heatmap(cm, annot=annot, fmt='', ax=ax, cmap='rocket_r')
-    #plt.savefig(filename)
-    plt.savefig('confusion_matrix_pbc_8_cropped_resnet101_fine_tuned_100e.png')
-    plt.savefig('confusion_matrix_pbc_8_cropped_resnet101_fine_tuned_100e.eps')
-    #plt.show()
-
-cm_analysis(y_test_list, y_pred_list, [i for i in rev_index] , ymap=None, figsize=(10,10))
-
-
-with open('report_pbc_8_cropped_resnet101_fine_tuned_100e.txt', 'w') as f:
-    sys.stdout = f # Change the standard output to the file we created.
-    print(report)
-    #sys.stdout = original_stdout # Reset the standard output to its original value
 
